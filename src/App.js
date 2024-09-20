295949ab34ec93778b5b42152c73b0eda13bf92e
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2024-09-20 22:45:46 +0200

    Refactor code - add timer to state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import { useState, useEffect } from "react";
 import Container from "./components/Container/Container";
 import FormattedTime from "./components/FormattedTime/FormattedTime";
-import Button from "./components/Button/Buton";
+import Button from "./components/Button/Button";
 
 const App = () => {
 
   const [time, setTime] = useState(0);
-  const [start, setStart] = useState(null)
+  const [timer, setTimer] = useState(false);
 
   useEffect(() => {
-    let timer;
-    if (start) {
-      timer = setInterval(() => { setTime(prevValue => prevValue + 10) }, 10);
+    let intervalId;
+    if (timer) {
+      intervalId = setInterval(() => { setTime(prevValue => prevValue + 10) }, 1);
     }
-      return () => {
-        if(timer) clearInterval(timer);
-      }
-    }, [start]
-  )
+    return () => intervalId && clearInterval(intervalId);
+  }, [timer]);
+
+  const handleStart = () => !timer && setTimer(true);
+
+  const handleStop = () => timer && setTimer(false);
+
+  const handleReset = () => {
+    handleStop();
+    setTime(0);
+  };
 
   return (
     <Container>
       <FormattedTime time={time} />
-      <Button  start={setStart} setTime={setTime} />
+      <Button
+        start={handleStart}
+        stop={handleStop}
+        reset={handleReset} />
     </Container>
   );
-}
+};
 
 export default App;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import styles from './Button.module.scss';
+
+const Button = props => {
+
+  return (
+    <div>
+      <button className={styles.button} onClick={props.start}>START</button>
+      <button className={styles.button} onClick={props.stop}>STOP</button>
+      <button className={styles.button} onClick={props.reset}>RESET</button>
+    </div>
+  );
+}
+
+export default Button;
